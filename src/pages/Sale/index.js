77b58c6d028cd0ad92aa7b3e1f9920a7bcdd04e6
WesleyVitor <wesleydemorais@outlook.com.br>
2022-01-24 10:37:46 -0300

    add: localstorage to persist data

--- src/pages/Sale/index.js
+++ src/pages/Sale/index.js
@@ -1,9 +1,16 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import Layout from '../../components/Layout';
 import "./styles.css";
 import Form from '../../components/Form';
 import Table from '../../components/Table';
+
+const storage = window.localStorage;
+
 const Sale = ()=>{
+
+    const handleLocalStorage = ()=>{
+        return JSON.parse(storage.getItem('db_sale')) ?? [];
+    }
 
     const initialSale = {
         id:0,
@@ -14,13 +21,19 @@ const Sale = ()=>{
         saleDate:"",
         deliveryDate:""
     }
-    const [sales, setSales] = useState([])
+    const [sales, setSales] = useState(handleLocalStorage);
 
     const addSale = sale => {
-		sale.id = sales.length + 1
-		setSales([ ...sales, sale ])
+		sale.id = sales.length + 1;
+		setSales([ ...sales, sale ]);
+        
+
 	}
-    console.log(sales);
+    
+    useEffect(()=>{
+        storage.setItem('db_sale',JSON.stringify(sales));
+    },[sales])
+
     return (
         <Layout>
             <div>
